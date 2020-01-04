fece6be4a359d5bf3afe8142d32f310c148bb418
Sabrina WANG <sabrina.wang.siyu@gmail.com>
2020-01-04 12:15:36 +0100

    add test code and test corpus in several languages

--- gui_tkinter_test9_window.py
+++ gui_tkinter_test9_window.py
@@ -1,21 +1,25 @@
-from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import asksaveasfilename
 from tkinter.messagebox import showerror
 
-import langdetect
 import re
 import io
 import os
 import jieba
+import jieba.posseg as pseg
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
+import langdetect
+import spacy
 from collections import Counter
 import pprint   # For proper print of sequences.
-import treetaggerwrapper
-import spacy
+import matplotlib
+matplotlib.use("TkAgg") 
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 # --- classes ---
 
@@ -46,6 +50,8 @@ class MyWindow:
         self.bigram_button = tk.Button(self.parent, text='BIGRAMS', command=self.bigram)
         self.bigram_button.pack()
 
+        self.grapheWindow_button = tk.Button(self.parent, text="POS GRAPHE", command=self.CreateNewWindow)
+        self.grapheWindow_button.pack()
 
         # # save data button
         # self.save_button = tk.Button(self.parent, text='SAVE DATA', command=self.file_save)
@@ -75,6 +81,11 @@ class MyWindow:
             self.text.insert('end', 'Language propability : \n\t' + str(language) + '\n--------------------\n')
             #self.text.insert('end', str(str(self.df)) + '\n')
 
+    def CreateNewWindow(self):
+        self.top = tk.Toplevel()
+        self.top.title("Graphe Stats")
+        
+
     def bigram(self):
         # create bigrams for non-chinese languages
         text = self.df
@@ -83,7 +94,7 @@ class MyWindow:
         if language == 'zh':
             res = bigram_ch_sort(text)
             #print(res)
-            self.text.insert('end', 'Top bigrams : \n')
+            self.text.insert('end', '--------------------\nTop bigrams : \n')
             for b in res[:40]:
                 self.text.insert('end', '\t'+ str(b) + '\n')
         else:
@@ -96,7 +107,7 @@ class MyWindow:
                 bigram_list.append(a[0]+' '+a[1])
             res=Counter(bigram_list).most_common(len(bigrams))
             #print(res)
-            self.text.insert('end', 'Top bigrams : \n')
+            self.text.insert('end', '--------------------\nTop bigrams : \n')
             for b in res[:40]:
                 self.text.insert('end', '\t'+ str(b) + '\n')
 
@@ -106,25 +117,41 @@ class MyWindow:
         langs = langdetect.detect_langs(text)
         language = str(langs[0])[:2]
         print(language)
+        # if chinese
         if language == "zh":
             wordlist_ch = tokenize_ch(text)
             res_ch=Counter(wordlist_ch).most_common(len(wordlist_ch))
-            self.text.insert('end', 'Top tokens : \n')
+            self.text.insert('end', '--------------------\nTop tokens : \n')
             self.text.insert('end', '\t'+ str(res_ch[:50]) + '\n')
+            list_tag_ch = pos_tag_ch(text)
+            self.text.insert('end', '\t'+ str(list_tag_ch) + '\n')
+        # if french
         elif language == "fr":
             wordlist_fr = tokenize(self.df, 'french')
-            print(wordlist_fr)
-            self.text.insert('end', 'Top tokens : \n')
+            #print(wordlist_fr)
+            self.text.insert('end', '--------------------\nTop tokens : \n')
             self.text.insert('end', '\t'+ str(wordlist_fr[:50]) + '\n')
-            nlp = spacy.load('fr_core_news_sm')
-            tag_fr = nlp(text)
+            
+            tag_fr = nlp_fr(text)
             tag_list = []
             for token in tag_fr:
                 print(token.text+"\t"+token.pos_+"\n")
                 tag_list.append(token.pos_)
             res=Counter(tag_list).most_common(len(tag_list))
-            self.text.insert('end', '\t'+ str(res) + '\n')
-            print(res)
+            #print(res)
+            self.text.insert('end', '--------------------\nPOS distribution: \n')
+            for a in res:
+                self.text.insert('end', '\t'+ str(a) + '\n')
+            labels, nb = get_value4graph(res)
+            tag_bar_graph(labels,nb)
+        # if english
+        elif language == 'en':
+            pass
+        # if german
+        elif language == 'de': 
+            pass
+        # if spanish
+        # if italien
         else: 
             # text = re.sub("\n"," ",self.df)
             # textlist = re.sub("[(),.]", "", text).split(' ')
@@ -158,9 +185,19 @@ def tokenize(text, langue):
 def tokenize_ch(text):
 	pattern=re.compile(u'[^\u4E00-\u9FA5]')
 	text=pattern.sub(r"", text)
-	wordlist_temp=list(jieba.cut(text))
+	wordlist_temp=list(jieba.cut(text)) # paddle mode
 	wordlist=[i.rstrip() for i in wordlist_temp if len(i)>=1]
 	return wordlist
+
+def pos_tag_ch(text):
+    list_tag_ch = []
+    dict_tag_ch = {}
+    words = pseg.cut(text,use_paddle=True) # paddle mode
+    for word, flag in words:
+        dict_tag_ch[word] = flag
+        list_tag_ch.append(flag)
+        #print(word+flag)
+    return list_tag_ch
 
 def bigram_ch(text):
 	wordlist=tokenize_ch(text)
@@ -176,13 +213,34 @@ def bigram_ch_sort(text):
     os.remove('temp.txt')
     return res
 	
+# --- graphes ---
+
+def get_value4graph(res):
+    labels = []
+    nb = []
+    for k,v in res:
+        labels.append(k)
+        nb.append(v)
+    return labels, nb
+
+def tag_bar_graph(labels, nb):
+    # this is for plotting purpose
+    index = np.arange(len(labels))
+    plt.bar(index, nb)
+    plt.xlabel('Tag', fontsize=5)
+    plt.ylabel('Nb', fontsize=5)
+    plt.xticks(index, labels, fontsize=5, rotation=30)
+    plt.title('Number of each POS')
+    plt.show()
 
 # --- main ---
 
 if __name__ == '__main__':
     root = tk.Tk()
     Title = root.title( "Siyu's Project")
-    root.geometry('700x500')
+    root.geometry('800x600')
+
+    nlp_fr = spacy.load('fr_core_news_sm')
     # add image to the interface
     # canvas = Canvas(width=500, height=300, bg='black')
     # image_file = PhotoImage(file='./bg.gif')  # 图片位置（相对路径，与.py文件同一文件夹下，也可以用绝对路径，需要给定图片具体绝对路径）
